99744c6da0be5785ac979422ffe42e4b63cf98b1
Ashwin <ashwinhole06@gmail.com>
2018-12-18 20:36:26 -0700

    Add Sorting&Searching Problem 1 & 2

---
+++ SortingAndSearching/P1_SortedMerge.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearching
+{
+    /* Sorted Merge: You are given two sorted arrays, A and B, where A has a large enough buffer at the
+        end to hold B. Write a method to merge B into A in sorted order.  */
+
+    public class P1_SortedMerge
+    {
+        public int[] MergedSortedArray(int[] a, int[] b, int totalCountInA, int totalCountInB)
+        {
+            int lastIndexA = totalCountInA - 1;
+            int lastIndexB = totalCountInB - 1;
+
+            int mergedCount = a.Length - 1;
+
+            while (lastIndexB >= 0)
+            {
+                if (lastIndexA >= 0 && a[lastIndexA] > b[lastIndexB])
+                {
+                    a[mergedCount] = a[lastIndexA];
+                    lastIndexA--;
+                }
+                else
+                {
+                    a[mergedCount] = b[lastIndexB];
+                    lastIndexB--;
+                }
+                mergedCount--;
+            }
+
+            return a;
+        }
+    }
+
+    [TestFixture]
+    public class TestSortedMerge
+    {
+        [Test]
+        public void SortedMerge_WithSortedArrayAAndB_ShouldReturnAContainingMergedAAndBSortedArray()
+        {
+            int[] A = new int[] { 1, 4, 5, 0, 0, 0 };
+            int[] B = new int[] { 2, 3, 6 };
+            int sizeA = 3, sizeB = 3;
+            int[] expected = new int[] { 1, 2, 3, 4, 5, 6 };
+            P1_SortedMerge sortedMerge = new P1_SortedMerge();
+
+            int[] actual = sortedMerge.MergedSortedArray(A, B, sizeA, sizeB);
+
+            expected.Should().BeEquivalentTo(actual);
+        }
+    }
+}
---
+++ SortingAndSearching/P2_GroupAnagrams.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearching
+{
+    /* Group Anagrams: Write a method to sort an array of strings so that all the anagrams are next to
+        each other. */
+
+    public class P2_GroupAnagrams
+    {
+        public List<string> GetGroupAnagrams(List<string> wordList)
+        {
+            Dictionary<string, List<string>> anagramDictionary = new Dictionary<string, List<string>>();
+
+            foreach (string word in wordList)
+            {
+                string sortedWord = String.Concat(word.OrderBy(c => c));
+
+                if (anagramDictionary.ContainsKey(sortedWord))
+                {
+                    anagramDictionary[sortedWord].Add(word);
+                }
+                else
+                {
+                    anagramDictionary.Add(sortedWord, new List<string>() { word });
+                }
+            }
+
+            List<string> anagramList = new List<string>();
+            foreach (KeyValuePair<string, List<string>> pair in anagramDictionary)
+            {
+                foreach (string word in pair.Value)
+                {
+                    anagramList.Add(word);
+                }
+            }
+
+            return anagramList;
+        }
+    }
+
+    [TestFixture]
+    public class TestGroupAnagrams
+    {
+        [Test]
+        public void GetGroupAnagrams_WithListOfWords_ShouldReturnAnagramList()
+        {
+            List<string> wordList = new List<string>() { "race", "pat", "acre", "tap", "care", "apt" };
+            List<string> expected = new List<string>() { "race", "acre", "care", "pat", "tap", "apt" };
+            P2_GroupAnagrams groupAnagrams = new P2_GroupAnagrams();
+
+            List<string> actual = groupAnagrams.GetGroupAnagrams(wordList);
+
+            expected.Should().BeEquivalentTo(actual);
+        }
+    }
+}
